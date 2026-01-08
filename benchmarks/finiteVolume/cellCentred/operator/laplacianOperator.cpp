ccdacb355f9e86013f5229d61a4e187bf93234f8
Dheeraj Raghunathan <64003931+dheerajraghunathan@users.noreply.github.com>
2026-01-08 09:48:01 +0100

    Added laplacianOperator

--- benchmarks/finiteVolume/cellCentred/operator/laplacianOperator.cpp
+++ benchmarks/finiteVolume/cellCentred/operator/laplacianOperator.cpp
@@ -11,32 +11,37 @@
 
 using Operator = NeoN::dsl::Operator;
 
-TEST_CASE("GradOperator::grad", "[bench]")
+TEST_CASE("LaplacianOperator::laplacian", "[bench]")
 {
     auto size = GENERATE(1 << 16, 1 << 17, 1 << 18, 1 << 19, 1 << 20);
     auto [execName, exec] = GENERATE(allAvailableExecutor());
 
     NeoN::UnstructuredMesh mesh = NeoN::create1DUniformMesh(exec, size);
 
+    // gamma is a SurfaceField<scalar>
+    auto surfaceBCs = fvcc::createCalculatedBCs<fvcc::SurfaceBoundary<NeoN::scalar>>(mesh);
+    fvcc::SurfaceField<NeoN::scalar> gamma(exec, "gamma", mesh, surfaceBCs);
+    NeoN::fill(gamma.internalVector(), 1.0);
+
     // Create a scalar field phi and initialise with 1.0
     auto volumeBCs = fvcc::createCalculatedBCs<fvcc::VolumeBoundary<NeoN::scalar>>(mesh);
     fvcc::VolumeField<NeoN::scalar> phi(exec, "phi", mesh, volumeBCs);
     NeoN::fill(phi.internalVector(), 1.0);
 
-    // Create a vector field to hold the gradient value
-    auto gradVecBCs = fvcc::createCalculatedBCs<fvcc::VolumeBoundary<NeoN::Vec3>>(mesh);
-    fvcc::VolumeField<NeoN::Vec3> gradPhi(exec, "gradPhi", mesh, gradVecBCs);
-    NeoN::fill(gradPhi.internalVector(), NeoN::zero<NeoN::Vec3>());
+    // Create a scalar field to hold the laplacian value - output field
+    fvcc::VolumeField<NeoN::scalar> lapPhi(exec, "lapPhi", mesh, volumeBCs);
+    NeoN::fill(lapPhi.internalVector(), 0.0);
 
     // capture the value of size as section name
     DYNAMIC_SECTION("" << size)
     {
         NeoN::Input input = NeoN::TokenList({std::string("Gauss"), std::string("linear")});
-        auto op = fvcc::GradOperator<NeoN::Vec3>(Operator::Type::Explicit, phi, input);
+        auto op =
+            fvcc::LaplacianOperator<NeoN::scalar>(Operator::Type::Explicit, gamma, phi, input);
 
         BENCHMARK(std::string(execName))
         {
-            return (op.explicitOperation(gradPhi.internalVector()));
+            return (op.explicitOperation(lapPhi.internalVector()));
         };
     }
 }
