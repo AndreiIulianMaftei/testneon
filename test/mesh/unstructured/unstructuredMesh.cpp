1fd9c128a7b704b1da227453ae980a7e41980519
chihtaw <wangchihta@gmail.com>
2026-04-22 15:41:32 +0200

    Rebase on develop branch and remove incompatible tests

--- test/mesh/unstructured/unstructuredMesh.cpp
+++ test/mesh/unstructured/unstructuredMesh.cpp
@@ -45,36 +45,6 @@ TEST_CASE("Unstructured Mesh")
         // Verify mesh points
         // bc  [   internal  ]  bc
         // 0.0 [ 0.25 | 0.50 ] 1.0
-        auto pointsExp = std::vector<NeoN::Vec3> {
-            {0.25, 0.0, 0.0}, {0.5, 0.0, 0.0}, {0.75, 0.0, 0.0}, {0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}
-        };
-        REQUIRE_THAT(pointsExp, IsEqualTo(mesh.points(), EqualInt()));
-
-        // Verify cell centers
-        auto cellCentresExp = std::vector<NeoN::Vec3> {
-            {0.125, 0.0, 0.0}, {0.375, 0.0, 0.0}, {0.625, 0.0, 0.0}, {0.875, 0.0, 0.0}
-        };
-        REQUIRE_THAT(cellCentresExp, IsEqualTo(mesh.cellCentres(), EqualInt()));
-
-        // Verify face owners
-        // |_3 0 |_0 1 |_1 2 |_2 3 |_4
-        auto faceOwnerExp = std::vector<NeoN::label> {0, 1, 2, 0, 3};
-        REQUIRE_THAT(faceOwnerExp, IsEqualTo(mesh.faceOwner(), EqualInt()));
-
-        // Verify face neighbors
-        // |_3 0 |_0 1 |_1 2 |_2 3 |_4
-        auto faceNeighbourExp = std::vector<NeoN::label> {1, 2, 3};
-        REQUIRE_THAT(faceNeighbourExp, IsEqualTo(mesh.faceNeighbour(), EqualInt()));
-
-        // Verify boundary mesh
-        auto faceCellsExp = std::vector<NeoN::localIdx> {0, 3};
-        REQUIRE_THAT(faceCellsExp, IsEqualTo(mesh.boundaryMesh().faceCells(), EqualInt()));
-
-        auto cnExp = std::vector<NeoN::Vec3> {{0.125, 0.0, 0.0}, {0.875, 0.0, 0.0}};
-        REQUIRE_THAT(cnExp, IsEqualTo(mesh.boundaryMesh().cn(), EqualInt()));
-
-        auto deltaExp = std::vector<NeoN::Vec3> {{-0.125, 0.0, 0.0}, {0.125, 0.0, 0.0}};
-        REQUIRE_THAT(deltaExp, IsEqualTo(mesh.boundaryMesh().delta(), EqualInt()));
         // Verify point count: (4+1)*(1+1)*(1+1) = 20
         auto hostPoints = mesh.points().copyToHost();
         REQUIRE(hostPoints.size() == 20);
