exasim-project/NeoN

make surface field contain only internal data

include/NeoN/finiteVolume/cellCentred/operators/ddtFluxCorr.hpp
@@ -40,26 +40,49 @@ inline void ddtFluxCorrBDF1Kernel(
     scalar dt
 )
 {
-    auto [outV, flux0V, uf0V, SfV] = views(
-        fluxCorr.internalVector(), flux0.internalVector(), uf0.internalVector(), mesh.faceAreas()
-    );
-
     const scalar a1 = scalar(1) / dt;
-    const auto n = outV.size();
+    const auto nInternalFaces = mesh.nInternalFaces();
+    const auto nBoundaryFaces = mesh.nBoundaryFaces();
 
-    parallelFor(
-        exec,
-        {size_t(0), n},
-        NEON_LAMBDA(const localIdx i) {
-            const auto d = (SfV[i] & uf0V[i]);
-            const auto corr = flux0V[i] - d;
+    // Internal faces
+    {
+        auto [outV, flux0V, uf0V, SfV] = views(
+            fluxCorr.internalVector(),
+            flux0.internalVector(),
+            uf0.internalVector(),
+            mesh.faceAreas()
+        );
+        parallelFor(
+            exec,
+            {size_t(0), static_cast<size_t>(nInternalFaces)},
+            NEON_LAMBDA(const localIdx i) {
+                const auto d = (SfV[i] & uf0V[i]);
+                const auto corr = flux0V[i] - d;
+                const scalar limiter = ddtFluxCorrLimiter(mag(flux0V[i]), mag(corr));
+                outV[i] = limiter * a1 * corr;
+            },
+            "ddtFluxCorr::BDF1::internal"
+        );
+    }
 
-            const scalar limiter = ddtFluxCorrLimiter(mag(flux0V[i]), mag(corr));
-
-            outV[i] = limiter * a1 * corr;
-        },
-        "ddtFluxCorr::BDF1"
-    );
+    // Boundary faces
+    {
+        auto outBV = fluxCorr.boundaryData().value().view();
+        auto flux0BV = flux0.boundaryData().value().view();
+        auto uf0BV = uf0.boundaryData().value().view();
+        auto SfV = mesh.faceAreas().view();
+        parallelFor(
+            exec,
+            {size_t(0), static_cast<size_t>(nBoundaryFaces)},
+            NEON_LAMBDA(const localIdx bfi) {
+                const auto d = (SfV[nInternalFaces + bfi] & uf0BV[bfi]);
+                const auto corr = flux0BV[bfi] - d;
+                const scalar limiter = ddtFluxCorrLimiter(mag(flux0BV[bfi]), mag(corr));
+                outBV[bfi] = limiter * a1 * corr;
+            },
+            "ddtFluxCorr::BDF1::boundary"
+        );
+    }
 }
 
 // ------------------------------------------------------------------
@@ -76,36 +99,66 @@ inline void ddtFluxCorrBDF2Kernel(
     scalar dt
 )
 {
-    auto [outV, flux0V, flux00V, uf0V, uf00V, SfV] = views(
-        fluxCorr.internalVector(),
-        flux0.internalVector(),
-        flux00.internalVector(),
-        uf0.internalVector(),
-        uf00.internalVector(),
-        mesh.faceAreas()
-    );
-
     const scalar a1 = 2.0 / dt;
     const scalar a2 = -0.5 / dt;
-    const auto n = outV.size();
+    const auto nInternalFaces = mesh.nInternalFaces();
+    const auto nBoundaryFaces = mesh.nBoundaryFaces();
 
-    parallelFor(
-        exec,
-        {size_t(0), n},
-        NEON_LAMBDA(const localIdx i) {
-            const auto d1 = (SfV[i] & uf0V[i]);
-            const auto corr1 = flux0V[i] - d1;
+    // Internal faces
+    {
+        auto [outV, flux0V, flux00V, uf0V, uf00V, SfV] = views(
+            fluxCorr.internalVector(),
+            flux0.internalVector(),
+            flux00.internalVector(),
+            uf0.internalVector(),
+            uf00.internalVector(),
+            mesh.faceAreas()
+        );
+        parallelFor(
+            exec,
+            {size_t(0), static_cast<size_t>(nInternalFaces)},
+            NEON_LAMBDA(const localIdx i) {
+                const auto d1 = (SfV[i] & uf0V[i]);
+                const auto corr1 = flux0V[i] - d1;
 
-            const auto d2 = (SfV[i] & uf00V[i]);
-            const auto corr2 = flux00V[i] - d2;
+                const auto d2 = (SfV[i] & uf00V[i]);
+                const auto corr2 = flux00V[i] - d2;
 
-            const scalar limiter1 = ddtFluxCorrLimiter(mag(flux0V[i]), mag(corr1));
-            const scalar limiter2 = ddtFluxCorrLimiter(mag(flux00V[i]), mag(corr2));
+                const scalar limiter1 = ddtFluxCorrLimiter(mag(flux0V[i]), mag(corr1));
+                const scalar limiter2 = ddtFluxCorrLimiter(mag(flux00V[i]), mag(corr2));
 
-            outV[i] = limiter1 * a1 * corr1 + limiter2 * a2 * corr2;
-        },
-        "ddtFluxCorr::BDF2"
-    );
+                outV[i] = limiter1 * a1 * corr1 + limiter2 * a2 * corr2;
+            },
+            "ddtFluxCorr::BDF2::internal"
+        );
+    }
+
+    // Boundary faces
+    {
+        auto outBV = fluxCorr.boundaryData().value().view();
+        auto flux0BV = flux0.boundaryData().value().view();
+        auto flux00BV = flux00.boundaryData().value().view();
+        auto uf0BV = uf0.boundaryData().value().view();
+        auto uf00BV = uf00.boundaryData().value().view();
+        auto SfV = mesh.faceAreas().view();
+        parallelFor(
+            exec,
+            {size_t(0), static_cast<size_t>(nBoundaryFaces)},
+            NEON_LAMBDA(const localIdx bfi) {
+                const auto d1 = (SfV[nInternalFaces + bfi] & uf0BV[bfi]);
+                const auto corr1 = flux0BV[bfi] - d1;
+
+                const auto d2 = (SfV[nInternalFaces + bfi] & uf00BV[bfi]);
+                const auto corr2 = flux00BV[bfi] - d2;
+
+                const scalar limiter1 = ddtFluxCorrLimiter(mag(flux0BV[bfi]), mag(corr1));
+                const scalar limiter2 = ddtFluxCorrLimiter(mag(flux00BV[bfi]), mag(corr2));
+
+                outBV[bfi] = limiter1 * a1 * corr1 + limiter2 * a2 * corr2;
+            },
+            "ddtFluxCorr::BDF2::boundary"
+        );
+    }
 }
 
 } // namespace detail

src/finiteVolume/cellCentred/operators/gaussGreenGrad.cpp
@@ -33,17 +33,18 @@ void computeGrad(
 
     auto surfGradPhi = out.view();
 
-    const auto [surfFaceCells, sBSf, surfPhif, surfOwner, surfNeighbour, faceAreaS, surfV] = views(
+    const auto [surfFaceCells, surfOwner, surfNeighbour, faceAreaS, surfV] = views(
         mesh.boundaryMesh().faceCells(),
-        mesh.boundaryMesh().sf(),
-        phif.internalVector(),
         mesh.faceOwner(),
         mesh.faceNeighbour(),
         mesh.faceAreas(),
         mesh.cellVolumes()
     );
+    const auto surfPhif = phif.internalVector().view();
+    const auto surfPhifB = phif.boundaryData().value().view();
 
     auto nInternalFaces = mesh.nInternalFaces();
+    auto nBoundaryFaces = mesh.nBoundaryFaces();
 
     // Green-Gauss gradient theorem: ∇φ_C = (1/V_C) * sum_f S_f * φ_f
     //
@@ -65,10 +66,10 @@ void computeGrad(
     // Boundary faces: only the owner cell is on this rank.
     parallelFor(
         exec,
-        {nInternalFaces, surfPhif.size()},
-        NEON_LAMBDA(const localIdx i) {
-            auto own = surfFaceCells[i - nInternalFaces];
-            Vec3 valueOwn = faceAreaS[i] * surfPhif[i]; // +S_f * φ_f (S_f outward from owner)
+        {0, nBoundaryFaces},
+        NEON_LAMBDA(const localIdx bfi) {
+            auto own = surfFaceCells[bfi];
+            Vec3 valueOwn = faceAreaS[nInternalFaces + bfi] * surfPhifB[bfi];
             Kokkos::atomic_add(&surfGradPhi[own], valueOwn);
         },
         "computeGradBoundary"
@@ -237,25 +238,25 @@ void computeGradTensor(
 
     auto gT = gradU.view();
 
-    const auto [UfAll, owner, nei, SfAll, V, bFaceCells] = views(
-        uf.internalVector(),
+    const auto [owner, nei, SfAll, V, bFaceCells] = views(
         mesh.faceOwner(),
         mesh.faceNeighbour(),
         mesh.faceAreas(),
         mesh.cellVolumes(),
         mesh.boundaryMesh().faceCells()
     );
+    const auto UfInt = uf.internalVector().view();
+    const auto UfBound = uf.boundaryData().value().view();
 
     const localIdx nInt = mesh.nInternalFaces();
-    const localIdx nBnd = mesh.boundaryMesh().offset().back();
-    const localIdx nFaces = nInt + nBnd;
+    const localIdx nBnd = mesh.nBoundaryFaces();
 
     parallelFor(
         exec,
         {0, nInt},
         NEON_LAMBDA(const localIdx f) {
             const Vec3 sf = SfAll[f];
-            const Vec3 faceU = UfAll[f];
+            const Vec3 faceU = UfInt[f];
             const auto o = owner[f];
             const auto n = nei[f];
             // gradU(row,col) += Sf[col] * U[row]  (Gauss-Green)
@@ -274,12 +275,11 @@ void computeGradTensor(
 
     parallelFor(
         exec,
-        {nInt, nFaces},
-        NEON_LAMBDA(const localIdx f) {
-            const localIdx bi = f - nInt;
+        {0, nBnd},
+        NEON_LAMBDA(const localIdx bi) {
             const auto o = bFaceCells[bi];
-            const Vec3 sf = SfAll[f];
-            const Vec3 faceU = UfAll[f];
+            const Vec3 sf = SfAll[nInt + bi];
+            const Vec3 faceU = UfBound[bi];
             for (size_t row = 0; row < 3; ++row)
             {
                 for (size_t col = 0; col < 3; ++col)

src/finiteVolume/cellCentred/operators/gaussGreenLaplacian.cpp
@@ -27,8 +27,10 @@ void computeLaplacianExp(
 
     const auto [result, faceArea, fnGrad, vol] =
         views(lapPhi, mesh.magFaceAreas(), faceNormalGrad.internalVector(), mesh.cellVolumes());
+    const auto fnGradB = faceNormalGrad.boundaryData().value().view();
 
     auto nInternalFaces = mesh.nInternalFaces();
+    auto nBoundaryFaces = mesh.nBoundaryFaces();
 
     // Green-Gauss Laplacian: ∇·(γ∇φ)_C = (1/V_C) * sum_f γ_f * |S_f| * (∂φ/∂n)_f
     //
@@ -59,11 +61,10 @@ void computeLaplacianExp(
     // Boundary faces: only the owner cell is on this rank.
     parallelFor(
         exec,
-        {nInternalFaces, fnGrad.size()},
-        NEON_LAMBDA(const localIdx i) {
-            auto own = surfFaceCells[i - nInternalFaces];
-            ValueType valueOwn =
-                faceArea[i] * fnGrad[i]; // +|S_f| * fnGrad (S_f outward from owner)
+        {0, nBoundaryFaces},
+        NEON_LAMBDA(const localIdx bfi) {
+            auto own = surfFaceCells[bfi];
+            ValueType valueOwn = faceArea[nInternalFaces + bfi] * fnGradB[bfi];
             Kokkos::atomic_add(&result[own], valueOwn);
         },
         "computeLaplacianExplicitBoundary"
@@ -102,13 +103,11 @@ void computeLaplacianBoundImpl(
     const auto exec = phi.exec();
     const auto& mesh = phi.mesh();
 
-    auto gammaV = gamma.internalVector().view();
+    const auto [magFaceArea, surfFaceCells] =
+        views(mesh.magFaceAreas(), mesh.boundaryMesh().faceCells());
 
-    const auto [magFaceArea, surfFaceCells, deltaCoeffs] = views(
-        mesh.magFaceAreas(),
-        mesh.boundaryMesh().faceCells(),
-        faceNormalGradient.deltaCoeffs().internalVector()
-    );
+    const auto bGammaV = gamma.boundaryData().value().view();
+    const auto bDeltaCoeffs = faceNormalGradient.deltaCoeffs().boundaryData().value().view();
 
     const auto ma = ls.faceToMatrixAddress()->view(ls.matrix().sparsity()->rowOffs().view());
 
@@ -126,12 +125,10 @@ void computeLaplacianBoundImpl(
 
     const auto nInternalFaces = mesh.nInternalFaces();
     const auto nBoundaryFaces = mesh.nBoundaryFaces();
-    auto totalFaces = nInternalFaces + nBoundaryFaces;
     parallelFor(
         exec,
-        {nInternalFaces, totalFaces},
-        NEON_LAMBDA(const localIdx facei) {
-            auto bfi = facei - nInternalFaces;
+        {0, nBoundaryFaces},
+        NEON_LAMBDA(const localIdx bfi) {
             auto ownRow = surfFaceCells[bfi];
 
             auto ownRowCoeff = operatorScaling[ownRow];
@@ -139,9 +136,9 @@ void computeLaplacianBoundImpl(
             auto refValFrac = valueFraction[bfi];
             auto refGradFrac = 1.0 - refValFrac;
 
-            auto flux = gammaV[facei] * magFaceArea[facei];
+            auto flux = bGammaV[bfi] * magFaceArea[nInternalFaces + bfi];
             auto fluxContrib =
-                flux * ownRowCoeff * refValFrac * deltaCoeffs[facei] * one<ValueType>();
+                flux * ownRowCoeff * refValFrac * bDeltaCoeffs[bfi] * one<ValueType>();
 
             // since upper triangular value is "outside" of system matrix
             // it is stored separately in bMatrix
@@ -155,9 +152,9 @@ void computeLaplacianBoundImpl(
             // The implicit valFrac2 * φ_C term is handled via fluxContrib above.
             // bweights converts the Dirichlet face value to a cell-to-face flux contribution;
             // the Neumann gradient correction (refGradient/δ) enters directly as a known increment.
-            auto valueRhs = flux * ownRowCoeff
-                          * (refValFrac * deltaCoeffs[facei] * refValue[bfi]
-                             + refGradFrac * refGradient[bfi]);
+            auto valueRhs =
+                flux * ownRowCoeff
+                * (refValFrac * bDeltaCoeffs[bfi] * refValue[bfi] + refGradFrac * refGradient[bfi]);
             Kokkos::atomic_sub(&rhs[ownRow], valueRhs);
             bRhs[bfi] += valueRhs;
         },

src/finiteVolume/cellCentred/operators/surfaceIntegrate.cpp
@@ -16,6 +16,7 @@ void surfaceIntegrate(
     View<const int> owner,
     View<const int> faceCells,
     View<const ValueType> flux,
+    View<const ValueType> bFlux,
     View<const scalar> v,
     View<ValueType> res,
     const dsl::Coeff operatorScaling
@@ -35,10 +36,10 @@ void surfaceIntegrate(
 
     parallelFor(
         exec,
-        {nInternalFaces, nInternalFaces + nBoundaryFaces},
-        NEON_LAMBDA(const localIdx i) {
-            auto own = faceCells[i - nInternalFaces];
-            Kokkos::atomic_add(&res[own], flux[i]);
+        {0, nBoundaryFaces},
+        NEON_LAMBDA(const localIdx bfi) {
+            auto own = faceCells[bfi];
+            Kokkos::atomic_add(&res[own], bFlux[bfi]);
         },
         "surfaceIntegrateBoundaryFaces"
     );
@@ -58,6 +59,7 @@ void surfaceIntegrate(
         View<const int>,                                                                           \
         View<const int>,                                                                           \
         View<const int>,                                                                           \
+        View<const TYPENAME>,                                                                      \
         View<const TYPENAME>,                                                                      \
         View<const scalar>,                                                                        \
         View<TYPENAME>,                                                                            \

AndreiIulianMaftei/testneon

include/NeoN/finiteVolume/cellCentred/operators/ddtFluxCorr.hpp
@@ -40,26 +40,49 @@ inline void ddtFluxCorrBDF1Kernel(
     scalar dt
 )
 {
-    auto [outV, flux0V, uf0V, SfV] = views(
-        fluxCorr.internalVector(), flux0.internalVector(), uf0.internalVector(), mesh.faceNormals()
-    );
-
     const scalar a1 = scalar(1) / dt;
-    const auto n = outV.size();
+    const auto nInternalFaces = mesh.nInternalFaces();
+    const auto nBoundaryFaces = mesh.nBoundaryFaces();
 
-    parallelFor(
-        exec,
-        {size_t(0), n},
-        NEON_LAMBDA(const localIdx i) {
-            const auto d = (SfV[i] & uf0V[i]);
-            const auto corr = flux0V[i] - d;
+    // Internal faces
+    {
+        auto [outV, flux0V, uf0V, SfV] = views(
+            fluxCorr.internalVector(),
+            flux0.internalVector(),
+            uf0.internalVector(),
+            mesh.faceNormals()
+        );
+        parallelFor(
+            exec,
+            {size_t(0), static_cast<size_t>(nInternalFaces)},
+            NEON_LAMBDA(const localIdx i) {
+                const auto d = (SfV[i] & uf0V[i]);
+                const auto corr = flux0V[i] - d;
+                const scalar limiter = ddtFluxCorrLimiter(mag(flux0V[i]), mag(corr));
+                outV[i] = limiter * a1 * corr;
+            },
+            "ddtFluxCorr::BDF1::internal"
+        );
+    }
 
-            const scalar limiter = ddtFluxCorrLimiter(mag(flux0V[i]), mag(corr));
-
-            outV[i] = limiter * a1 * corr;
-        },
-        "ddtFluxCorr::BDF1"
-    );
+    // Boundary faces
+    {
+        auto outBV = fluxCorr.boundaryData().value().view();
+        auto flux0BV = flux0.boundaryData().value().view();
+        auto uf0BV = uf0.boundaryData().value().view();
+        auto SfV = mesh.faceNormals().view();
+        parallelFor(
+            exec,
+            {size_t(0), static_cast<size_t>(nBoundaryFaces)},
+            NEON_LAMBDA(const localIdx bfi) {
+                const auto d = (SfV[nInternalFaces + bfi] & uf0BV[bfi]);
+                const auto corr = flux0BV[bfi] - d;
+                const scalar limiter = ddtFluxCorrLimiter(mag(flux0BV[bfi]), mag(corr));
+                outBV[bfi] = limiter * a1 * corr;
+            },
+            "ddtFluxCorr::BDF1::boundary"
+        );
+    }
 }
 
 // ------------------------------------------------------------------
@@ -76,36 +99,66 @@ inline void ddtFluxCorrBDF2Kernel(
     scalar dt
 )
 {
-    auto [outV, flux0V, flux00V, uf0V, uf00V, SfV] = views(
-        fluxCorr.internalVector(),
-        flux0.internalVector(),
-        flux00.internalVector(),
-        uf0.internalVector(),
-        uf00.internalVector(),
-        mesh.faceNormals()
-    );
-
     const scalar a1 = 2.0 / dt;
     const scalar a2 = -0.5 / dt;
-    const auto n = outV.size();
+    const auto nInternalFaces = mesh.nInternalFaces();
+    const auto nBoundaryFaces = mesh.nBoundaryFaces();
 
-    parallelFor(
-        exec,
-        {size_t(0), n},
-        NEON_LAMBDA(const localIdx i) {
-            const auto d1 = (SfV[i] & uf0V[i]);
-            const auto corr1 = flux0V[i] - d1;
+    // Internal faces
+    {
+        auto [outV, flux0V, flux00V, uf0V, uf00V, SfV] = views(
+            fluxCorr.internalVector(),
+            flux0.internalVector(),
+            flux00.internalVector(),
+            uf0.internalVector(),
+            uf00.internalVector(),
+            mesh.faceNormals()
+        );
+        parallelFor(
+            exec,
+            {size_t(0), static_cast<size_t>(nInternalFaces)},
+            NEON_LAMBDA(const localIdx i) {
+                const auto d1 = (SfV[i] & uf0V[i]);
+                const auto corr1 = flux0V[i] - d1;
 
-            const auto d2 = (SfV[i] & uf00V[i]);
-            const auto corr2 = flux00V[i] - d2;
+                const auto d2 = (SfV[i] & uf00V[i]);
+                const auto corr2 = flux00V[i] - d2;
 
-            const scalar limiter1 = ddtFluxCorrLimiter(mag(flux0V[i]), mag(corr1));
-            const scalar limiter2 = ddtFluxCorrLimiter(mag(flux00V[i]), mag(corr2));
+                const scalar limiter1 = ddtFluxCorrLimiter(mag(flux0V[i]), mag(corr1));
+                const scalar limiter2 = ddtFluxCorrLimiter(mag(flux00V[i]), mag(corr2));
 
-            outV[i] = limiter1 * a1 * corr1 + limiter2 * a2 * corr2;
-        },
-        "ddtFluxCorr::BDF2"
-    );
+                outV[i] = limiter1 * a1 * corr1 + limiter2 * a2 * corr2;
+            },
+            "ddtFluxCorr::BDF2::internal"
+        );
+    }
+
+    // Boundary faces
+    {
+        auto outBV = fluxCorr.boundaryData().value().view();
+        auto flux0BV = flux0.boundaryData().value().view();
+        auto flux00BV = flux00.boundaryData().value().view();
+        auto uf0BV = uf0.boundaryData().value().view();
+        auto uf00BV = uf00.boundaryData().value().view();
+        auto SfV = mesh.faceNormals().view();
+        parallelFor(
+            exec,
+            {size_t(0), static_cast<size_t>(nBoundaryFaces)},
+            NEON_LAMBDA(const localIdx bfi) {
+                const auto d1 = (SfV[nInternalFaces + bfi] & uf0BV[bfi]);
+                const auto corr1 = flux0BV[bfi] - d1;
+
+                const auto d2 = (SfV[nInternalFaces + bfi] & uf00BV[bfi]);
+                const auto corr2 = flux00BV[bfi] - d2;
+
+                const scalar limiter1 = ddtFluxCorrLimiter(mag(flux0BV[bfi]), mag(corr1));
+                const scalar limiter2 = ddtFluxCorrLimiter(mag(flux00BV[bfi]), mag(corr2));
+
+                outBV[bfi] = limiter1 * a1 * corr1 + limiter2 * a2 * corr2;
+            },
+            "ddtFluxCorr::BDF2::boundary"
+        );
+    }
 }
 
 } // namespace detail

src/finiteVolume/cellCentred/operators/gaussGreenGrad.cpp
@@ -33,25 +33,18 @@ void computeGrad(
 
     auto surfGradPhi = out.view();
 
-    const auto
-        [boundaryFaceOwners,
-         boundaryFaceNormals,
-         surfPhif,
-         faceOwners,
-         faceNeighbors,
-         faceNormals,
-         surfV] =
-            views(
-                mesh.boundaryMesh().faceOwners(),
-                mesh.boundaryMesh().faceNormals(),
-                phif.internalVector(),
-                mesh.faceOwners(),
-                mesh.faceNeighbors(),
-                mesh.faceNormals(),
-                mesh.cellVolumes()
-            );
+    const auto [boundaryFaceOwners, faceOwners, faceNeighbors, faceAreaS, surfV] = views(
+        mesh.boundaryMesh().faceOwners(),
+        mesh.faceOwners(),
+        mesh.faceNeighbors(),
+        mesh.faceNormals(),
+        mesh.cellVolumes()
+    );
+    const auto surfPhif = phif.internalVector().view();
+    const auto surfPhifB = phif.boundaryData().value().view();
 
     auto nInternalFaces = mesh.nInternalFaces();
+    auto nBoundaryFaces = mesh.nBoundaryFaces();
 
     // Green-Gauss gradient theorem: ∇φ_C = (1/V_C) * sum_f S_f * φ_f
     //
@@ -63,7 +56,7 @@ void computeGrad(
         exec,
         {0, nInternalFaces},
         NEON_LAMBDA(const localIdx i) {
-            Vec3 flux = faceNormals[i] * surfPhif[i];
+            Vec3 flux = faceAreaS[i] * surfPhif[i];
             Kokkos::atomic_add(&surfGradPhi[faceOwners[i]], flux);    // +S_f * φ_f
             Kokkos::atomic_sub(&surfGradPhi[faceNeighbors[i]], flux); // −S_f * φ_f
         },
@@ -73,10 +66,10 @@ void computeGrad(
     // Boundary faces: only the owner cell is on this rank.
     parallelFor(
         exec,
-        {nInternalFaces, surfPhif.size()},
-        NEON_LAMBDA(const localIdx i) {
-            auto own = boundaryFaceOwners[i - nInternalFaces];
-            Vec3 valueOwn = faceNormals[i] * surfPhif[i]; // +S_f * φ_f (S_f outward from owner)
+        {0, nBoundaryFaces},
+        NEON_LAMBDA(const localIdx bfi) {
+            auto own = boundaryFaceOwners[bfi];
+            Vec3 valueOwn = faceAreaS[nInternalFaces + bfi] * surfPhifB[bfi];
             Kokkos::atomic_add(&surfGradPhi[own], valueOwn);
         },
         "computeGradBoundary"
@@ -245,25 +238,25 @@ void computeGradTensor(
 
     auto gT = gradU.view();
 
-    const auto [UfAll, owner, nei, SfAll, V, bFaceCells] = views(
-        uf.internalVector(),
+    const auto [owner, nei, SfAll, V, bFaceCells] = views(
         mesh.faceOwners(),
         mesh.faceNeighbors(),
         mesh.faceNormals(),
         mesh.cellVolumes(),
         mesh.boundaryMesh().faceOwners()
     );
+    const auto UfInt = uf.internalVector().view();
+    const auto UfBound = uf.boundaryData().value().view();
 
     const localIdx nInt = mesh.nInternalFaces();
-    const localIdx nBnd = mesh.boundaryMesh().offset().back();
-    const localIdx nFaces = nInt + nBnd;
+    const localIdx nBnd = mesh.nBoundaryFaces();
 
     parallelFor(
         exec,
         {0, nInt},
         NEON_LAMBDA(const localIdx f) {
             const Vec3 sf = SfAll[f];
-            const Vec3 faceU = UfAll[f];
+            const Vec3 faceU = UfInt[f];
             const auto o = owner[f];
             const auto n = nei[f];
             // gradU(row,col) += Sf[col] * U[row]  (Gauss-Green)
@@ -282,12 +275,11 @@ void computeGradTensor(
 
     parallelFor(
         exec,
-        {nInt, nFaces},
-        NEON_LAMBDA(const localIdx f) {
-            const localIdx bi = f - nInt;
+        {0, nBnd},
+        NEON_LAMBDA(const localIdx bi) {
             const auto o = bFaceCells[bi];
-            const Vec3 sf = SfAll[f];
-            const Vec3 faceU = UfAll[f];
+            const Vec3 sf = SfAll[nInt + bi];
+            const Vec3 faceU = UfBound[bi];
             for (size_t row = 0; row < 3; ++row)
             {
                 for (size_t col = 0; col < 3; ++col)

src/finiteVolume/cellCentred/operators/gaussGreenLaplacian.cpp
@@ -27,8 +27,10 @@ void computeLaplacianExp(
 
     const auto [result, faceArea, fnGrad, vol] =
         views(lapPhi, mesh.faceAreas(), faceNormalGrad.internalVector(), mesh.cellVolumes());
+    const auto fnGradB = faceNormalGrad.boundaryData().value().view();
 
     auto nInternalFaces = mesh.nInternalFaces();
+    auto nBoundaryFaces = mesh.nBoundaryFaces();
 
     // Green-Gauss Laplacian: ∇·(γ∇φ)_C = (1/V_C) * sum_f γ_f * |S_f| * (∂φ/∂n)_f
     //
@@ -59,11 +61,10 @@ void computeLaplacianExp(
     // Boundary faces: only the owner cell is on this rank.
     parallelFor(
         exec,
-        {nInternalFaces, fnGrad.size()},
-        NEON_LAMBDA(const localIdx i) {
-            auto own = boundaryFaceOwners[i - nInternalFaces];
-            ValueType valueOwn =
-                faceArea[i] * fnGrad[i]; // +|S_f| * fnGrad (S_f outward from owner)
+        {0, nBoundaryFaces},
+        NEON_LAMBDA(const localIdx bfi) {
+            auto own = boundaryFaceOwners[bfi];
+            ValueType valueOwn = faceArea[nInternalFaces + bfi] * fnGradB[bfi];
             Kokkos::atomic_add(&result[own], valueOwn);
         },
         "computeLaplacianExplicitBoundary"
@@ -102,13 +103,11 @@ void computeLaplacianBoundImpl(
     const auto exec = phi.exec();
     const auto& mesh = phi.mesh();
 
-    auto gammaV = gamma.internalVector().view();
+    const auto [magFaceArea, boundaryFaceOwners] =
+        views(mesh.faceAreas(), mesh.boundaryMesh().faceOwners());
 
-    const auto [magFaceArea, boundaryFaceOwners, deltaCoeffs] = views(
-        mesh.faceAreas(),
-        mesh.boundaryMesh().faceOwners(),
-        faceNormalGradient.deltaCoeffs().internalVector()
-    );
+    const auto bGammaV = gamma.boundaryData().value().view();
+    const auto bDeltaCoeffs = faceNormalGradient.deltaCoeffs().boundaryData().value().view();
 
     const auto ma = ls.faceToMatrixAddress()->view(ls.matrix().sparsity()->rowOffs().view());
 
@@ -126,12 +125,10 @@ void computeLaplacianBoundImpl(
 
     const auto nInternalFaces = mesh.nInternalFaces();
     const auto nBoundaryFaces = mesh.nBoundaryFaces();
-    auto totalFaces = nInternalFaces + nBoundaryFaces;
     parallelFor(
         exec,
-        {nInternalFaces, totalFaces},
-        NEON_LAMBDA(const localIdx facei) {
-            auto bfi = facei - nInternalFaces;
+        {0, nBoundaryFaces},
+        NEON_LAMBDA(const localIdx bfi) {
             auto ownRow = boundaryFaceOwners[bfi];
 
             auto ownRowCoeff = operatorScaling[ownRow];
@@ -139,9 +136,9 @@ void computeLaplacianBoundImpl(
             auto refValFrac = valueFraction[bfi];
             auto refGradFrac = 1.0 - refValFrac;
 
-            auto flux = gammaV[facei] * magFaceArea[facei];
+            auto flux = bGammaV[bfi] * magFaceArea[nInternalFaces + bfi];
             auto fluxContrib =
-                flux * ownRowCoeff * refValFrac * deltaCoeffs[facei] * one<ValueType>();
+                flux * ownRowCoeff * refValFrac * bDeltaCoeffs[bfi] * one<ValueType>();
 
             // since upper triangular value is "outside" of system matrix
             // it is stored separately in bMatrix
@@ -155,9 +152,9 @@ void computeLaplacianBoundImpl(
             // The implicit valFrac2 * φ_C term is handled via fluxContrib above.
             // bweights converts the Dirichlet face value to a cell-to-face flux contribution;
             // the Neumann gradient correction (refGradient/δ) enters directly as a known increment.
-            auto valueRhs = flux * ownRowCoeff
-                          * (refValFrac * deltaCoeffs[facei] * refValue[bfi]
-                             + refGradFrac * refGradient[bfi]);
+            auto valueRhs =
+                flux * ownRowCoeff
+                * (refValFrac * bDeltaCoeffs[bfi] * refValue[bfi] + refGradFrac * refGradient[bfi]);
             Kokkos::atomic_sub(&rhs[ownRow], valueRhs);
             bRhs[bfi] += valueRhs;
         },

src/finiteVolume/cellCentred/operators/surfaceIntegrate.cpp
@@ -16,6 +16,7 @@ void surfaceIntegrate(
     View<const int> owners,
     View<const int> faceOwners,
     View<const ValueType> flux,
+    View<const ValueType> bFlux,
     View<const scalar> v,
     View<ValueType> res,
     const dsl::Coeff operatorScaling
@@ -35,10 +36,10 @@ void surfaceIntegrate(
 
     parallelFor(
         exec,
-        {nInternalFaces, nInternalFaces + nBoundaryFaces},
-        NEON_LAMBDA(const localIdx i) {
-            auto own = faceOwners[i - nInternalFaces];
-            Kokkos::atomic_add(&res[own], flux[i]);
+        {0, nBoundaryFaces},
+        NEON_LAMBDA(const localIdx bfi) {
+            auto own = faceOwners[bfi];
+            Kokkos::atomic_add(&res[own], bFlux[bfi]);
         },
         "surfaceIntegrateBoundaryFaces"
     );
@@ -58,6 +59,7 @@ void surfaceIntegrate(
         View<const int>,                                                                           \
         View<const int>,                                                                           \
         View<const int>,                                                                           \
+        View<const TYPENAME>,                                                                      \
         View<const TYPENAME>,                                                                      \
         View<const scalar>,                                                                        \
         View<TYPENAME>,                                                                            \