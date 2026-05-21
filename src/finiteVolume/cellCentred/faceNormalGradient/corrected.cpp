exasim-project/NeoN

Add corrected and limitedCorrected faceNormalGradient schemes
Implement non-orthogonality correction for surface normal gradients:

- Rename updateNonOrthDeltaCoeffs(Vec3) to updateNonOrthCorrectionVec3s
  in GeometrySchemeFactory and BasicGeometryScheme to match its actual
  purpose, and implement the correction vector computation:
  corrVec[f] = n̂ - delta * nonOrthDeltaCoeff

- Add corrected scheme: snGrad = nonOrthDeltaCoeff*(phi_N - phi_O)
  + corrVec · (w*grad(phi)_O + (1-w)*grad(phi)_N)

- Add limitedCorrected scheme: same as corrected but with a limiter
  that bounds the correction relative to the orthogonal part.
  Accepts limitCoeff from TokenList or Dictionary (default 0.333).

- Both schemes use a fused single kernel per face group and reuse
  GaussGreenGrad for cell-centred gradient computation.

- Add unit tests on a 1D uniform mesh (orthogonal, corrVec=0) verifying
  that corrected and limitedCorrected reduce to uncorrected.

src/finiteVolume/cellCentred/faceNormalGradient/corrected.cpp
@@ -1,0 +1,96 @@
+// SPDX-FileCopyrightText: 2023 - 2026 NeoN authors
+//
+// SPDX-License-Identifier: MIT
+
+#include <memory>
+#include <type_traits>
+
+#include "NeoN/core/error.hpp"
+#include "NeoN/finiteVolume/cellCentred/faceNormalGradient/corrected.hpp"
+
+namespace NeoN::finiteVolume::cellCentred
+{
+
+template<typename ValueType>
+void computeCorrectedFaceNormalGrad(
+    const VolumeField<ValueType>& volField,
+    const std::shared_ptr<GeometryScheme> geometryScheme,
+    SurfaceField<ValueType>& surfaceField
+)
+{
+    if constexpr (std::is_same_v<ValueType, scalar>)
+    {
+        const UnstructuredMesh& mesh = surfaceField.mesh();
+        const auto& exec = surfaceField.exec();
+
+        // Compute cell-centred gradient via Gauss-Green
+        GaussGreenGrad gradScheme(exec, mesh);
+        VolumeField<Vec3> gradPhi = gradScheme.grad(volField);
+
+        const auto [owner, neighbour, surfFaceCells] =
+            views(mesh.faceOwner(), mesh.faceNeighbour(), mesh.boundaryMesh().faceCells());
+
+        const auto
+            [phif,
+             phifB,
+             phi,
+             phiBCValue,
+             nonOrthDeltaCoeffs,
+             nonOrthDeltaCoeffsB,
+             weights,
+             corrVec] =
+                views(
+                    surfaceField.internalVector(),
+                    surfaceField.boundaryData().value(),
+                    volField.internalVector(),
+                    volField.boundaryData().value(),
+                    geometryScheme->nonOrthDeltaCoeffs().internalVector(),
+                    geometryScheme->nonOrthDeltaCoeffs().boundaryData().value(),
+                    geometryScheme->weights().internalVector(),
+                    geometryScheme->nonOrthCorrectionVec3s().internalVector()
+                );
+
+        const auto gradPhiV = gradPhi.internalVector().view();
+
+        auto nInternalFaces = mesh.nInternalFaces();
+        auto nBoundaryFaces = mesh.nBoundaryFaces();
+
+        NeoN::parallelFor(
+            exec,
+            {0, nInternalFaces},
+            NEON_LAMBDA(const localIdx facei) {
+                scalar ortho =
+                    nonOrthDeltaCoeffs[facei] * (phi[neighbour[facei]] - phi[owner[facei]]);
+                Vec3 interpGrad = weights[facei] * gradPhiV[owner[facei]]
+                                + (scalar(1) - weights[facei]) * gradPhiV[neighbour[facei]];
+                phif[facei] = ortho + (corrVec[facei] & interpGrad);
+            },
+            "computeCorrectedFaceNormalGradInternal"
+        );
+
+        // corrVec is zero at boundaries — boundary snGrad reduces to the uncorrected form.
+        NeoN::parallelFor(
+            exec,
+            {0, nBoundaryFaces},
+            NEON_LAMBDA(const localIdx bfi) {
+                auto own = surfFaceCells[bfi];
+                phifB[bfi] = nonOrthDeltaCoeffsB[bfi] * (phiBCValue[bfi] - phi[own]);
+            },
+            "computeCorrectedFaceNormalGradBoundary"
+        );
+    }
+    else
+    {
+        NF_ERROR_EXIT("corrected snGrad for Vec3 fields is not implemented: "
+                      "requires tensor gradient support");
+    }
+}
+
+#define NF_DECLARE_COMPUTE_CORRECTED_FNG(TYPENAME)                                                 \
+    template void computeCorrectedFaceNormalGrad<                                                  \
+        TYPENAME>(const VolumeField<TYPENAME>&, const std::shared_ptr<GeometryScheme>, SurfaceField<TYPENAME>&)
+
+NF_DECLARE_COMPUTE_CORRECTED_FNG(scalar);
+NF_DECLARE_COMPUTE_CORRECTED_FNG(Vec3);
+
+} // namespace NeoN

src/finiteVolume/cellCentred/faceNormalGradient/limitedCorrected.cpp
@@ -1,0 +1,109 @@
+// SPDX-FileCopyrightText: 2023 - 2026 NeoN authors
+//
+// SPDX-License-Identifier: MIT
+
+#include <memory>
+#include <type_traits>
+
+#include "NeoN/core/error.hpp"
+#include "NeoN/finiteVolume/cellCentred/faceNormalGradient/limitedCorrected.hpp"
+
+namespace NeoN::finiteVolume::cellCentred
+{
+
+template<typename ValueType>
+void computeLimitedCorrectedFaceNormalGrad(
+    const VolumeField<ValueType>& volField,
+    const std::shared_ptr<GeometryScheme> geometryScheme,
+    scalar limitCoeff,
+    SurfaceField<ValueType>& surfaceField
+)
+{
+    if constexpr (std::is_same_v<ValueType, scalar>)
+    {
+        const UnstructuredMesh& mesh = surfaceField.mesh();
+        const auto& exec = surfaceField.exec();
+
+        // Compute cell-centred gradient via Gauss-Green
+        GaussGreenGrad gradScheme(exec, mesh);
+        VolumeField<Vec3> gradPhi = gradScheme.grad(volField);
+
+        const auto [owner, neighbour, surfFaceCells] =
+            views(mesh.faceOwner(), mesh.faceNeighbour(), mesh.boundaryMesh().faceCells());
+
+        const auto
+            [phif,
+             phifB,
+             phi,
+             phiBCValue,
+             nonOrthDeltaCoeffs,
+             nonOrthDeltaCoeffsB,
+             weights,
+             corrVec] =
+                views(
+                    surfaceField.internalVector(),
+                    surfaceField.boundaryData().value(),
+                    volField.internalVector(),
+                    volField.boundaryData().value(),
+                    geometryScheme->nonOrthDeltaCoeffs().internalVector(),
+                    geometryScheme->nonOrthDeltaCoeffs().boundaryData().value(),
+                    geometryScheme->weights().internalVector(),
+                    geometryScheme->nonOrthCorrectionVec3s().internalVector()
+                );
+
+        const auto gradPhiV = gradPhi.internalVector().view();
+
+        auto nInternalFaces = mesh.nInternalFaces();
+        auto nBoundaryFaces = mesh.nBoundaryFaces();
+        const scalar lc = limitCoeff;
+        const scalar oneMinusLc = scalar(1) - lc;
+
+        NeoN::parallelFor(
+            exec,
+            {0, nInternalFaces},
+            NEON_LAMBDA(const localIdx facei) {
+                scalar ortho =
+                    nonOrthDeltaCoeffs[facei] * (phi[neighbour[facei]] - phi[owner[facei]]);
+                Vec3 interpGrad = weights[facei] * gradPhiV[owner[facei]]
+                                + (scalar(1) - weights[facei]) * gradPhiV[neighbour[facei]];
+                scalar corr = corrVec[facei] & interpGrad;
+
+                // Limiter: bounds the correction relative to the orthogonal part
+                scalar absCorr = std::abs(corr);
+                scalar limiter =
+                    (absCorr > scalar(0)) ? std::min(
+                        lc * std::abs(ortho) / (oneMinusLc * absCorr + ROOTVSMALL), scalar(1)
+                    )
+                                          : scalar(1);
+
+                phif[facei] = ortho + limiter * corr;
+            },
+            "computeLimitedCorrectedFaceNormalGradInternal"
+        );
+
+        // corrVec is zero at boundaries — boundary snGrad reduces to the uncorrected form.
+        NeoN::parallelFor(
+            exec,
+            {0, nBoundaryFaces},
+            NEON_LAMBDA(const localIdx bfi) {
+                auto own = surfFaceCells[bfi];
+                phifB[bfi] = nonOrthDeltaCoeffsB[bfi] * (phiBCValue[bfi] - phi[own]);
+            },
+            "computeLimitedCorrectedFaceNormalGradBoundary"
+        );
+    }
+    else
+    {
+        NF_ERROR_EXIT("limitedCorrected snGrad for Vec3 fields is not implemented: "
+                      "requires tensor gradient support");
+    }
+}
+
+#define NF_DECLARE_COMPUTE_LIMITED_CORRECTED_FNG(TYPENAME)                                         \
+    template void computeLimitedCorrectedFaceNormalGrad<                                           \
+        TYPENAME>(const VolumeField<TYPENAME>&, const std::shared_ptr<GeometryScheme>, scalar, SurfaceField<TYPENAME>&)
+
+NF_DECLARE_COMPUTE_LIMITED_CORRECTED_FNG(scalar);
+NF_DECLARE_COMPUTE_LIMITED_CORRECTED_FNG(Vec3);
+
+} // namespace NeoN

AndreiIulianMaftei/testneon

src/finiteVolume/cellCentred/faceNormalGradient/corrected.cpp
@@ -1,0 +1,96 @@
+// SPDX-FileCopyrightText: 2023 - 2026 NeoN authors
+//
+// SPDX-License-Identifier: MIT
+
+#include <memory>
+#include <type_traits>
+
+#include "NeoN/core/error.hpp"
+#include "NeoN/finiteVolume/cellCentred/faceNormalGradient/corrected.hpp"
+
+namespace NeoN::finiteVolume::cellCentred
+{
+
+template<typename ValueType>
+void computeCorrectedFaceNormalGrad(
+    const VolumeField<ValueType>& volField,
+    const std::shared_ptr<GeometryScheme> geometryScheme,
+    SurfaceField<ValueType>& surfaceField
+)
+{
+    if constexpr (std::is_same_v<ValueType, scalar>)
+    {
+        const UnstructuredMesh& mesh = surfaceField.mesh();
+        const auto& exec = surfaceField.exec();
+
+        // Compute cell-centred gradient via Gauss-Green
+        GaussGreenGrad gradScheme(exec, mesh);
+        VolumeField<Vec3> gradPhi = gradScheme.grad(volField);
+
+        const auto [owners, neighbors, boundaryFaceOwners] =
+            views(mesh.faceOwners(), mesh.faceNeighbors(), mesh.boundaryMesh().faceOwners());
+
+        const auto
+            [phif,
+             phifB,
+             phi,
+             phiBCValue,
+             nonOrthDeltaCoeffs,
+             nonOrthDeltaCoeffsB,
+             weights,
+             corrVec] =
+                views(
+                    surfaceField.internalVector(),
+                    surfaceField.boundaryData().value(),
+                    volField.internalVector(),
+                    volField.boundaryData().value(),
+                    geometryScheme->nonOrthDeltaCoeffs().internalVector(),
+                    geometryScheme->nonOrthDeltaCoeffs().boundaryData().value(),
+                    geometryScheme->weights().internalVector(),
+                    geometryScheme->nonOrthCorrectionVec3s().internalVector()
+                );
+
+        const auto gradPhiV = gradPhi.internalVector().view();
+
+        auto nInternalFaces = mesh.nInternalFaces();
+        auto nBoundaryFaces = mesh.nBoundaryFaces();
+
+        NeoN::parallelFor(
+            exec,
+            {0, nInternalFaces},
+            NEON_LAMBDA(const localIdx facei) {
+                scalar ortho =
+                    nonOrthDeltaCoeffs[facei] * (phi[neighbors[facei]] - phi[owners[facei]]);
+                Vec3 interpGrad = weights[facei] * gradPhiV[owners[facei]]
+                                + (scalar(1) - weights[facei]) * gradPhiV[neighbors[facei]];
+                phif[facei] = ortho + (corrVec[facei] & interpGrad);
+            },
+            "computeCorrectedFaceNormalGradInternal"
+        );
+
+        // corrVec is zero at boundaries — boundary snGrad reduces to the uncorrected form.
+        NeoN::parallelFor(
+            exec,
+            {0, nBoundaryFaces},
+            NEON_LAMBDA(const localIdx bfi) {
+                auto own = boundaryFaceOwners[bfi];
+                phifB[bfi] = nonOrthDeltaCoeffsB[bfi] * (phiBCValue[bfi] - phi[own]);
+            },
+            "computeCorrectedFaceNormalGradBoundary"
+        );
+    }
+    else
+    {
+        NF_ERROR_EXIT("corrected snGrad for Vec3 fields is not implemented: "
+                      "requires tensor gradient support");
+    }
+}
+
+#define NF_DECLARE_COMPUTE_CORRECTED_FNG(TYPENAME)                                                 \
+    template void computeCorrectedFaceNormalGrad<                                                  \
+        TYPENAME>(const VolumeField<TYPENAME>&, const std::shared_ptr<GeometryScheme>, SurfaceField<TYPENAME>&)
+
+NF_DECLARE_COMPUTE_CORRECTED_FNG(scalar);
+NF_DECLARE_COMPUTE_CORRECTED_FNG(Vec3);
+
+} // namespace NeoN

src/finiteVolume/cellCentred/faceNormalGradient/limitedCorrected.cpp
@@ -1,0 +1,109 @@
+// SPDX-FileCopyrightText: 2023 - 2026 NeoN authors
+//
+// SPDX-License-Identifier: MIT
+
+#include <memory>
+#include <type_traits>
+
+#include "NeoN/core/error.hpp"
+#include "NeoN/finiteVolume/cellCentred/faceNormalGradient/limitedCorrected.hpp"
+
+namespace NeoN::finiteVolume::cellCentred
+{
+
+template<typename ValueType>
+void computeLimitedCorrectedFaceNormalGrad(
+    const VolumeField<ValueType>& volField,
+    const std::shared_ptr<GeometryScheme> geometryScheme,
+    scalar limitCoeff,
+    SurfaceField<ValueType>& surfaceField
+)
+{
+    if constexpr (std::is_same_v<ValueType, scalar>)
+    {
+        const UnstructuredMesh& mesh = surfaceField.mesh();
+        const auto& exec = surfaceField.exec();
+
+        // Compute cell-centred gradient via Gauss-Green
+        GaussGreenGrad gradScheme(exec, mesh);
+        VolumeField<Vec3> gradPhi = gradScheme.grad(volField);
+
+        const auto [owners, neighbors, boundaryFaceOwners] =
+            views(mesh.faceOwners(), mesh.faceNeighbors(), mesh.boundaryMesh().faceOwners());
+
+        const auto
+            [phif,
+             phifB,
+             phi,
+             phiBCValue,
+             nonOrthDeltaCoeffs,
+             nonOrthDeltaCoeffsB,
+             weights,
+             corrVec] =
+                views(
+                    surfaceField.internalVector(),
+                    surfaceField.boundaryData().value(),
+                    volField.internalVector(),
+                    volField.boundaryData().value(),
+                    geometryScheme->nonOrthDeltaCoeffs().internalVector(),
+                    geometryScheme->nonOrthDeltaCoeffs().boundaryData().value(),
+                    geometryScheme->weights().internalVector(),
+                    geometryScheme->nonOrthCorrectionVec3s().internalVector()
+                );
+
+        const auto gradPhiV = gradPhi.internalVector().view();
+
+        auto nInternalFaces = mesh.nInternalFaces();
+        auto nBoundaryFaces = mesh.nBoundaryFaces();
+        const scalar lc = limitCoeff;
+        const scalar oneMinusLc = scalar(1) - lc;
+
+        NeoN::parallelFor(
+            exec,
+            {0, nInternalFaces},
+            NEON_LAMBDA(const localIdx facei) {
+                scalar ortho =
+                    nonOrthDeltaCoeffs[facei] * (phi[neighbors[facei]] - phi[owners[facei]]);
+                Vec3 interpGrad = weights[facei] * gradPhiV[owners[facei]]
+                                + (scalar(1) - weights[facei]) * gradPhiV[neighbors[facei]];
+                scalar corr = corrVec[facei] & interpGrad;
+
+                // Limiter: bounds the correction relative to the orthogonal part
+                scalar absCorr = std::abs(corr);
+                scalar limiter =
+                    (absCorr > scalar(0)) ? std::min(
+                        lc * std::abs(ortho) / (oneMinusLc * absCorr + ROOTVSMALL), scalar(1)
+                    )
+                                          : scalar(1);
+
+                phif[facei] = ortho + limiter * corr;
+            },
+            "computeLimitedCorrectedFaceNormalGradInternal"
+        );
+
+        // corrVec is zero at boundaries — boundary snGrad reduces to the uncorrected form.
+        NeoN::parallelFor(
+            exec,
+            {0, nBoundaryFaces},
+            NEON_LAMBDA(const localIdx bfi) {
+                auto own = boundaryFaceOwners[bfi];
+                phifB[bfi] = nonOrthDeltaCoeffsB[bfi] * (phiBCValue[bfi] - phi[own]);
+            },
+            "computeLimitedCorrectedFaceNormalGradBoundary"
+        );
+    }
+    else
+    {
+        NF_ERROR_EXIT("limitedCorrected snGrad for Vec3 fields is not implemented: "
+                      "requires tensor gradient support");
+    }
+}
+
+#define NF_DECLARE_COMPUTE_LIMITED_CORRECTED_FNG(TYPENAME)                                         \
+    template void computeLimitedCorrectedFaceNormalGrad<                                           \
+        TYPENAME>(const VolumeField<TYPENAME>&, const std::shared_ptr<GeometryScheme>, scalar, SurfaceField<TYPENAME>&)
+
+NF_DECLARE_COMPUTE_LIMITED_CORRECTED_FNG(scalar);
+NF_DECLARE_COMPUTE_LIMITED_CORRECTED_FNG(Vec3);
+
+} // namespace NeoN